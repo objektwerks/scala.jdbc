aaee4d6fdd2fa76fdd0b27427f0eb0a84a3c921e
objektwerks <objektwerks@runbox.com>
2023-09-27 09:30:13 -0400

    enhanced store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -30,9 +30,21 @@ private object Store:
 
 final class Store(config: Config):
   private val ds: DataSource = Store.createDataSource(config)
-  private val addTodoQuery = Using( ds.getConnection().prepareStatement("insert into todo(task) values(?)", Statement.RETURN_GENERATED_KEYS) ) { ps => ps }.get
-  private val updateTodoQuery = Using( ds.getConnection().prepareStatement("update todo set task = ? where id = ?") ) { ps => ps }.get
-  private val listTodosQuery = Using( ds.getConnection().prepareStatement("select * from todo") ) { ps => ps }.get
+  private val addTodoQuery = Using(
+    ds
+      .getConnection()
+      .prepareStatement("insert into todo(task) values(?)", Statement.RETURN_GENERATED_KEYS) 
+  ) { ps => ps }.get
+  private val updateTodoQuery = Using(
+    ds
+      .getConnection()
+      .prepareStatement("update todo set task = ? where id = ?")
+  ) { ps => ps }.get
+  private val listTodosQuery = Using(
+    ds
+      .getConnection()
+      .prepareStatement("select * from todo")
+  ) { ps => ps }.get
 
   def addTodo(todo: Todo): Todo =
     addTodoQuery.setString(1, todo.task)
